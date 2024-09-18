e531dfbaea90c431e5433910f2e0c64ba7436709
rodrigo <rodrigo.rgtic@gmail.com>
2024-09-18 11:25:36 -0300

    refactor

--- src/controllers/questions-controller.ts
+++ src/controllers/questions-controller.ts
@@ -1,36 +1,20 @@
-import { NextFunction, Request, Response } from "express"
+import { Request, Response } from "express"
 
 class QuestionsController {
-  async index(request: Request, response: Response, next: NextFunction) {
-    try {
-      return response.json()
-    } catch (error) {
-      next(error)
-    }
+  async index(request: Request, response: Response) {
+    return response.json()
   }
 
-  async create(request: Request, response: Response, next: NextFunction) {
-    try {
-      return response.status(201).json()
-    } catch (error) {
-      next(error)
-    }
+  async create(request: Request, response: Response) {
+    return response.status(201).json()
   }
 
-  async update(request: Request, response: Response, next: NextFunction) {
-    try {
-      return response.json()
-    } catch (error) {
-      next(error)
-    }
+  async update(request: Request, response: Response) {
+    return response.json()
   }
 
-  async remove(request: Request, response: Response, next: NextFunction) {
-    try {
-      return response.json()
-    } catch (error) {
-      next(error)
-    }
+  async remove(request: Request, response: Response) {
+    return response.json()
   }
 }
 
